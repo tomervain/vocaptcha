ff1a141f0b9fe0ddf7c16e0ad37fa87b47831004
tomervain <43002689+tomervain@users.noreply.github.com>
2020-06-16 13:03:34 +0300

    minor changes

---
+++ src/main.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+
+if __name__ == "__main__":
+    window = tk.Tk(screenName='VoCAPTCHA Demo')
+    greeting = tk.Label(text="Hello, Tkinter", width=60, height=10)
+    greeting.pack()
+    window.mainloop()
--- src/test.py
+++ src/test.py
@@ -13,32 +13,32 @@ from lib.qa_generator import generate_qa
 #     print(s, '\n')
 
 # test phonetic comparison
-# s1 = 'Hannah'
-# s2 = 'Ana'
-# soundex = pp.RefinedSoundex()
+s1 = 'Brian'
+s2 = 'Ryan'
+soundex = pp.Soundex()
 
-# print(f'{s1}: {soundex.phonetics(s1)}')
-# print(f'{s2}: {soundex.phonetics(s2)}')
-# print(f'distance: {soundex.distance(s1,s2)}')
+print(f'{s1}: {soundex.phonetics(s1)}')
+print(f'{s2}: {soundex.phonetics(s2)}')
+print(f'distance: {soundex.distance(s1,s2)}')
 
-# similar = True if soundex.distance(s1,s2) < 2 else False
-# print(f'similar: {similar}')
+similar = True if soundex.distance(s1,s2) < 2 else False
+print(f'similar: {similar}')
 
 # test full workflow:
-sentences = generate()
-qa_pairs = []
-for sentence in sentences:
-    print(sentence.sentence_str)
-    qa_pairs += generate_qa(sentence)
+# sentences = generate()
+# qa_pairs = []
+# for sentence in sentences:
+#     print(sentence.sentence_str)
+#     qa_pairs += generate_qa(sentence)
 
-shuffle(qa_pairs)
-pairs = sample(qa_pairs, k=3)
+# shuffle(qa_pairs)
+# pairs = sample(qa_pairs, k=3)
 
-print('\nQuestions:')
-for i, p in enumerate(pairs):
-    print(f'{i+1}. {p[0]}')
+# print('\nQuestions:')
+# for i, p in enumerate(pairs):
+#     print(f'{i+1}. {p[0]}')
 
-print('\nAnswers:')
-for i, p in enumerate(pairs):
-    print(f'{i+1}. {p[1]}')
+# print('\nAnswers:')
+# for i, p in enumerate(pairs):
+#     print(f'{i+1}. {p[1]}')
 
